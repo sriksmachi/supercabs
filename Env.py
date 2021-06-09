c4e18746aeb7c1b106aeb0b1eec123801c0b6b87
Srikanth Machiraju <vism@microsoft.com>
2021-06-10 00:31:54 +0530

    updated few env behaviors, added test code and git ignore

--- Env.py
+++ Env.py
@@ -1,5 +1,6 @@
 # Import routines
 
+from os import stat
 import numpy as np
 import math
 import random
@@ -28,25 +29,21 @@ class CabDriver():
         self.action_space = spaces.Tuple((spaces.Discrete(5),spaces.Discrete(5)))
         self.state_space = (self.location, self.time, self.day)
         self.state_init = (self.location, self.time, self.day)
-
+        self.time_matrix = np.load('TM.npy')
         # Start the first round
         self.reset()
 
 
     ## Encoding state (or state-action) for NN input
-
-    def state_encod_arch1(self, state):
-        """convert the state into a vector so that it can be fed to the NN. This method converts a given state into a vector format. Hint: The vector is of size m + t + d."""
-
-        return state_encod
-
+    # def state_encod_arch1(self, state):
+    #     """convert the state into a vector so that it can be fed to the NN. This method converts a given state into a vector format. Hint: The vector is of size m + t + d."""
+    #     return state_encod
 
     # Use this function if you are using architecture-2 
-    # def state_encod_arch2(self, state, action):
-    #     """convert the (state-action) into a vector so that it can be fed to the NN. This method converts a given state-action pair into a vector format. Hint: The vector is of size m + t + d + m + m."""
-
-        
-    #     return state_encod
+    def state_encod_arch2(self, state, action):
+        """convert the (state-action) into a vector so that it can be fed to the NN. This method converts a given state-action pair into a vector format. Hint: The vector is of size m + t + d + m + m."""
+        state_encod = np.array([1, 2, 3]) # No clue what this means should figure out
+        return state_encod
 
 
     ## Getting number of requests
@@ -77,16 +74,29 @@ class CabDriver():
 
     def reward_func(self, state, action, Time_matrix):
         """Takes in state, action and Time-matrix and returns the reward"""
+        current_location = state[0]
+        start_location = action[0]
+        end_location = action[1]
+        hour_of_day = state[1]
+        day_of_week = state[2]
+        revenue = R * Time_matrix[start_location][end_location][hour_of_day][day_of_week]
+        cost_of_trip = C * (Time_matrix[start_location][end_location][hour_of_day][day_of_week] + Time_matrix[current_location][start_location][hour_of_day][day_of_week])
+        cost_of_idle = C * (Time_matrix[0][0][hour_of_day][day_of_week])
+        reward =  revenue -  cost_of_trip - cost_of_idle
         return reward
-
-
-
 
     def next_state_func(self, state, action, Time_matrix):
         """Takes state and action as input and returns next state"""
-        return next_state
-
-
+        end_location = action[1]
+        start_location = action[0]
+        hour_of_day = state[1]
+        day_of_week = state[2]
+        next_state_location = end_location # this is the destination city
+        # time in next state = current time + time it takes to reach next state
+        next_state_hour_of_day = hour_of_day + Time_matrix[start_location][end_location][hour_of_day][day_of_week]
+        # day in next state = next day ? if current time + time it takes to reach next state is > 24 or current day
+        next_state_day_of_week = (day_of_week + 1) if next_state_hour_of_day > 24 else day_of_week
+        return (next_state_location, next_state_hour_of_day, next_state_day_of_week)
 
 
     def reset(self):
