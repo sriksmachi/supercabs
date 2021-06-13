d50cf897f776d9b1a691fa454dead5540608b701
Srikanth Machiraju <vism@microsoft.com>
2021-06-13 19:01:15 +0530

    updated code to get reward if the driver is at the same location as of the action

--- Env.py
+++ Env.py
@@ -26,7 +26,7 @@ class CabDriver():
         self.day = np.random.choice(np.arange(0, d))
         self.time = np.random.choice(np.arange(0,t))
         self.location = np.random.choice(np.arange(0,m))
-        self.action_space = list(permutations(range(0,5), 2)) 
+        self.action_space = [(p,q) for p in range(m) for q in range(m) if p!=q or p==0]
         self.state_space = (self.location, self.time, self.day)
         self.state_init = (self.location, self.time, self.day)
         self.time_matrix = np.load('TM.npy')
@@ -68,19 +68,20 @@ class CabDriver():
         if requests >15:
             requests =15
 
-        possible_actions_index = random.sample(range(1, (m-1)*m +1), requests) # (0,0) is not considered as customer request
-        
-        # print(f'possible actions..{possible_actions_index}')
-        # print(f'actions space..{self.action_space}')
-        
-        actions = [self.action_space[i-1] for i in possible_actions_index]
-        possible_actions_index.append((m-1)*m +1) # this is so that the NN learns to pick the no_idle as well based on Q value
-        actions.append((0,0)) # adding the no-ride action
+        possible_actions_index = [0] + random.sample(range(1, (m-1)*m +1), requests) # (0,0) is not considered as customer request
+        actions = [self.action_space[i] for i in possible_actions_index]
         return possible_actions_index,actions   
 
+    def get_updated_time(self, time, day):
+        if(time > 23):
+            time = time % 24
+            day = day + 1
+            if (day >=7):
+                day = day % 7
+        return time, day
 
     def step(self, state, action, Time_matrix, steps):
-        """Takes in state, action, Time-matrix and steps and returns the state, action, reward, next_state, trip_hours
+        """Takes in state, action, Time-matrix and steps and returns the state, reward, next_state, trip_hours
         
         Args:
         - state : current state
@@ -88,27 +89,43 @@ class CabDriver():
         - time matrix: the function of time taken for travel
         - steps: steps is the hours completed so far. 
         """
+        # print(f'step action..{action}')
         # when user is at A 10 AM On Monday, and receives a request for (B,C)
         current_location = state[0] # A
         start_location = action[0] # B
         end_location = action[1] # C
         current_hour_of_day = state[1] # 10
         current_day_of_week = state[2] # 1
+        reward = 0
 
         # check if the action is to no_ride
         if action[0] == 0 and action[1] == 0:
 
             next_state_location = current_location
-            next_state_hour_of_day = current_hour_of_day
+            next_state_hour_of_day = current_hour_of_day + 1
             next_state_day_of_week = current_day_of_week
+            next_state_hour_of_day, next_state_day_of_week = self.get_updated_time(next_state_hour_of_day, next_state_day_of_week)
+            
             hours_of_trip = 1 # 1 hour the driver is idle
             reward = -C
         
+        elif action[0] == state[0]: # if the drive is at the pick up location
+
+            next_state_location = end_location # this is the destination city
+            start_location_to_end_location = int(Time_matrix[start_location][end_location][current_hour_of_day][current_day_of_week])
+            next_state_hour_of_day = current_hour_of_day + start_location_to_end_location
+            next_state_day_of_week = current_day_of_week
+            
+            # the time it takes to complete the trip from the start hour could be more than 24 hours, hence
+            next_state_hour_of_day, next_state_day_of_week = self.get_updated_time(next_state_hour_of_day, next_state_day_of_week)
+            
+            hours_of_trip = start_location_to_end_location
+
         else:
 
             next_state_location = end_location # this is the destination city
 
-            # hour of day in next state = current time + time it takes to reach start location + time it takes to reach destination
+            # the time it takes to reach the pick up location
             current_location_to_start_location = int(Time_matrix[current_location][start_location][current_hour_of_day][current_day_of_week])
 
             # If it takes 1 hour to each reach B from A, this is 11, 
@@ -116,36 +133,30 @@ class CabDriver():
             start_day_of_week = current_day_of_week
 
             # this can be more than 23 in which case its next day
-            if(start_hour_of_day > 23):
-                start_hour_of_day = start_hour_of_day - 24
-                start_day_of_week = current_day_of_week + 1
-                if (start_day_of_week >=7):
-                    start_day_of_week = start_day_of_week - 7
+            start_hour_of_day, start_day_of_week = self.get_updated_time(start_hour_of_day, start_day_of_week)
 
             # this is the time it takes to reach B -> C
             start_location_to_end_location = int(Time_matrix[start_location][end_location][start_hour_of_day][start_day_of_week])
             
+            # hour of day in next state = current time + time it takes to reach start location + time it takes to reach destination
             next_state_hour_of_day = current_hour_of_day + current_location_to_start_location + start_location_to_end_location
             next_state_day_of_week = start_day_of_week
 
             # the time it takes to complete the trip from the start hour could be more than 24 hours, hence
-            if(next_state_hour_of_day > 23):
-                next_state_hour_of_day = next_state_hour_of_day - 24
-                next_state_day_of_week = next_state_day_of_week + 1
-                if (next_state_day_of_week >=7):
-                    next_state_day_of_week = next_state_day_of_week - 7
+            next_state_hour_of_day, next_state_day_of_week = self.get_updated_time(next_state_hour_of_day, next_state_day_of_week)
 
             hours_of_trip = current_location_to_start_location + start_location_to_end_location
 
             # reward calculation
             # reward = revenue (B, C) - cost of (A-> B and B-> C)
-            revenue = R * int(Time_matrix[start_location][end_location][start_hour_of_day][start_day_of_week])
+            revenue = R * start_location_to_end_location
             cost_of_trip = C * (hours_of_trip)
             reward =  revenue - cost_of_trip
         
         # print(f'hours_of_trip...{hours_of_trip}')
         next_state = (next_state_location, next_state_hour_of_day, next_state_day_of_week)
-        return state, action, reward, next_state, hours_of_trip
+        return state, reward, next_state, hours_of_trip
+
 
 
     def reset(self):
